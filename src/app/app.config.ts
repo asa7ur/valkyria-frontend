25a8e8adc1e61697a54ff9826e51bd46b3f5ab8e
Garik Asatryan <asa7ur@proton.me>
2026-01-04 22:29:15 +0100

    Minor changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,16 @@
-import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import {provideRouter, withComponentInputBinding} from '@angular/router';
-import {provideHttpClient} from '@angular/common/http';
+
 import {routes} from './app.routes';
+import {provideHttpClient} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({eventCoalescing: true}),
-    provideRouter(routes, withComponentInputBinding()),
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(
+      routes,
+      withComponentInputBinding()
+    ),
     provideHttpClient()
   ]
 };
