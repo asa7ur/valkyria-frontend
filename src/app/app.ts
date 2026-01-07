c53b5adf64feef1337633c18a7d99406278cf6f5
Garik Asatryan <asa7ur@proton.me>
2026-01-07 11:58:28 +0100

    Adding starting animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component, signal} from '@angular/core';
+import {Component, signal, OnInit, inject} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 
 @Component({
@@ -7,15 +7,23 @@ import {RouterOutlet} from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('valkyria-frontend');
 
+  // Estado para controlar la existencia de la pantalla de carga
   showSplash = signal(true);
+  // Estado para activar la animación de desvanecimiento (fade-out)
+  isFadingOut = signal(false);
 
   ngOnInit() {
-    // Forzamos que la pantalla dure, por ejemplo, 2.5 segundos
+    // 1. Duración del logo pulsando (2.5 segundos)
     setTimeout(() => {
-      this.showSplash.set(false);
-    }, 2500);
+      this.isFadingOut.set(true); // Iniciamos el desvanecimiento suave
+
+      // 2. Esperamos a que termine la transición de CSS (800ms) para eliminarlo del DOM
+      setTimeout(() => {
+        this.showSplash.set(false);
+      }, 800);
+    }, 2000);
   }
 }
