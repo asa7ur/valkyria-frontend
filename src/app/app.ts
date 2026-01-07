5c14f9ab29268ee8663301d81a7213ccab68ec76
Garik Asatryan <asa7ur@proton.me>
2026-01-07 11:41:07 +0100

    Adding starting animation

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,13 @@ import {RouterOutlet} from '@angular/router';
 })
 export class App {
   protected readonly title = signal('valkyria-frontend');
+
+  showSplash = signal(true);
+
+  ngOnInit() {
+    // Forzamos que la pantalla dure, por ejemplo, 2.5 segundos
+    setTimeout(() => {
+      this.showSplash.set(false);
+    }, 2500);
+  }
 }
