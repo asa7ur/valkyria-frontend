d706e009b3c6c7ad9d3ffbf9e04c49f885be00e5
Garik Asatryan <asa7ur@proton.me>
2026-01-16 11:22:29 +0100

    Minor changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import {Component, signal, OnInit, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {ConfirmDialog} from './shared/components/confirm-dialog/confirm-dialog';
 
@@ -8,23 +8,5 @@ import {ConfirmDialog} from './shared/components/confirm-dialog/confirm-dialog';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  protected readonly title = signal('valkyria-frontend');
-
-  // Estado para controlar la existencia de la pantalla de carga
-  showSplash = signal(true);
-  // Estado para activar la animación de desvanecimiento (fade-out)
-  isFadingOut = signal(false);
-
-  ngOnInit() {
-    // 1. Duración del logo pulsando (2.5 segundos)
-    setTimeout(() => {
-      this.isFadingOut.set(true); // Iniciamos el desvanecimiento suave
-
-      // 2. Esperamos a que termine la transición de CSS (800ms) para eliminarlo del DOM
-      setTimeout(() => {
-        this.showSplash.set(false);
-      }, 800);
-    }, 2000);
-  }
+export class App {
 }
--- src/app/layout/components/main-layout/main-layout.ts
+++ src/app/layout/components/main-layout/main-layout.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, signal, OnInit} from '@angular/core';
 import {Header} from '../../header/header';
 import {RouterOutlet} from '@angular/router';
 import {Footer} from '../../footer/footer';
@@ -13,6 +13,20 @@ import {Footer} from '../../footer/footer';
   templateUrl: './main-layout.html',
   styles: ``,
 })
-export class MainLayout {
+export class MainLayout implements OnInit {
+  showSplash = signal(true);
+  // Estado para activar la animación de desvanecimiento (fade-out)
+  isFadingOut = signal(false);
 
+  ngOnInit() {
+    // 1. Duración del logo pulsando (2.5 segundos)
+    setTimeout(() => {
+      this.isFadingOut.set(true); // Iniciamos el desvanecimiento suave
+
+      // 2. Esperamos a que termine la transición de CSS (800ms) para eliminarlo del DOM
+      setTimeout(() => {
+        this.showSplash.set(false);
+      }, 800);
+    }, 2000);
+  }
 }
