0d99a3f6b3f2763162f354302ce984ab4362ea50
Garik Asatryan <asa7ur@proton.me>
2026-04-04 17:01:38 +0200

    Improved Security when the token times out

--- src/app/core/interceptors/auth.ts
+++ src/app/core/interceptors/auth.ts
@@ -1,16 +1,28 @@
-import {HttpInterceptorFn} from '@angular/common/http';
+import {HttpErrorResponse, HttpInterceptorFn} from '@angular/common/http';
+import {inject} from '@angular/core';
+import {Router} from '@angular/router';
+import {catchError, throwError} from 'rxjs';
 
 export const auth: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('auth_token');
+  const router = inject(Router);
 
+  let authReq = req;
   if (token) {
-    const authReq = req.clone({
+    authReq = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(authReq);
   }
 
-  return next(req);
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        localStorage.removeItem('auth_token');
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    })
+  )
 };
